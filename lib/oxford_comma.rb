6cd182b0b35be52734895c6d1cd91edec5742a95
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-04-03 22:33:17 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,12 @@
 def oxford_comma(array)
-  last_element = array.slice!(-1)
-  and_added = "and " + last_element
-  if array.count == 0
-    oxford_comma_string = array.push(last_element).join(" ")
-  elsif array.count == 1
-    oxford_comma_string = array.push(and_added).join(" ")
+  add_and = "and " + array.slice(-1)
+  if array.length == 1
+    oxford_comma_string = array.join(" ")
+  elsif array.length == 2
+    array[-1] = add_and
+    oxford_comma_string = array.join(" ")
   else
-    oxford_comma_string = array.push(and_added).join(", ")
+    array[-1] = add_and
+    oxford_comma_string = array.join(", ")
   end
 end

d69637f6b03bfdc5ff1e5111e292fcb01bc76d18
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-04-03 22:32:32 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,21 +1,8 @@
-# def oxford_comma(array)
-#   last_element = array.slice!(-1)
-#   and_added = "and " + last_element
-#   if array.count == 0
-#     oxford_comma_string = array.push(last_element).join(" ")
-#   elsif array.count == 1
-#     oxford_comma_string = array.push(and_added).join(" ")
-#   else
-#     oxford_comma_string = array.push(and_added).join(", ")
-#   end
-# end
-
-
 def oxford_comma(array)
   add_and = "and " + array.slice(-1)
-  if array.count == 1
+  if array.length == 1
     oxford_comma_string = array.join(" ")
-  elsif array.count == 2
+  elsif array.length == 2
     array[-1] = add_and
     oxford_comma_string = array.join(" ")
   else
